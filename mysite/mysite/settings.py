0644109121afeb84f2064cbde8d16a2b425c9612
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-19 01:08:22 +0900

    MySQLへの接続を処理

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # データベースにsqlite3を使いますよみたいなやつ
+        'NAME': 'test', # データベース名
+        'USER': 'root', # DBへ接続するユーザー名
+        'PASSWORD': 'ZIId34eTdASj8AD4', # DBへ接続するユーザIDのパスワード
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'",
+        },
+        'TEST': {
+            'NAME': 'test_sample'
+        }
     }
 }
 
