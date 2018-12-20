1fe61cb4866c62e1dcb08bd7a4c2a932e5b6a0b1
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-21 05:18:15 +0900

    template機能を使ってwebページを呼び出すことに成功

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index")
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render())
 
 # 新しいビュー関数を追加
 def detail(request, question_id):
