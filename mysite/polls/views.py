da417abdcbf55eef06b7526550f3b983d0773e65
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-22 03:19:47 +0900

    コンテキスト使ってみた。あと質問のテキスト日本語OK確認

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,9 +4,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Question
+
 def index(request):
+    text = "直近の質問5つを新しい順に表示"
+    list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    return HttpResponse(template.render())
+    subject = "数学"
+    score = "100"
+    context = {
+      'title_text': text, # title_textは変数展開{{}}でindex.htmlに埋め込まれている
+      'latest_question_list': list, # latest_question_listはfor文の形で埋め込み。
+      'subject': subject,
+      'score': score,
+    }
+    return HttpResponse(template.render(context, request))
 
 # 新しいビュー関数を追加
 def detail(request, question_id):
