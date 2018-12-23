2dedbb0f35aa933341d4cdcf0afa11db3789263f
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-24 04:17:33 +0900

    グラフのパターンを増やす

--- mysite/use_plt/views.py
+++ mysite/use_plt/views.py
@@ -10,10 +10,32 @@ def index(request):
 
 def set_plt(pk):
     if pk is 1:
+        fig = plt.figure()
         x = np.array([1, 2, 3, 4, 5])
         y = np.array([10, 20, 30, 40, 50])
         plt.plot(x, y)
         plt.title("simple")
+
+    if pk is 2:
+        fig = plt.figure()
+        x = np.linspace(-2, 2, 100)
+        y = x ** 2
+        plt.plot(x, y)
+        plt.title(r'$y={x}^2$')
+
+    if pk is 3:
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        X = np.arange(-4, 4, 0.25)
+        Y = np.arange(-4, 4, 0.25)
+        X, Y = np.meshgrid(X, Y)
+        R = np.sqrt(7 * (X ** 2) + 2 * (Y ** 2))
+        Z = np.sin(R)
+        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.hot)
+        ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.cm.hot)
+        ax.set_zlim(-2, 2)
+        plt.title(r'${Z}^2 = 7{X}^2 + 2{Y}^2$')
 
 def plt_to_svg():
     buf = io.BytesIO()
