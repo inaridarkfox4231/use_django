20e7722c0a33d4495988f51a5e5ec03614a2936f
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2018-12-25 00:13:36 +0900

    テストの書き方を学ぶ

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+# 未来の日付は最近ではないので、testを書いて確かめる。
+
+import datetime
+from django.utils import timezone
+
+from .models import Question
+
+print('test polls')
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days = 30)
+        future_question = Question(pub_date = time)
+        self.assertIs(future_question.was_published_recently(), False)
+        # Falseじゃないとエラーを返すみたいな。
+
+# これを実行する。はいでました。
+# 日付が過去である、という条件を加えて再実行する。
